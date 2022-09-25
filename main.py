e4e4c31ecf271e0e0e13eb40bbdd69886c58b3c2
Santiago Paiz <santiago.paiz@gmail.com>
2022-09-25 17:07:51 +0200

    fix: :fire: remove unnecessary modules

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from scp_scrapper import multiple_scp_scrape
-from database_manager import save_df
 
-
-scps_I = [x for x in range(2,1000)]
+scps_I = [x for x in range(2,10)]
 
 new_df = multiple_scp_scrape(scps_I)
 
-new_df.to_csv('test.csv')
+new_df.to_csv('test2.csv')
 # save_df(new_df)
